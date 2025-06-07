adefbdd5f9d59fd8d1c12b9ca1e88d0e2a540ba0
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-06-08 00:35:05 +0530

    finished setting up initial setup

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@repo/ui/components/ui/button";
-
 export default function Page() {
-  return (
-    <main>
-      <Button>Click me</Button>
-    </main>
-  );
+  return <main>Docs</main>;
 }
